19d128357378f1c47557b53beaddbdfd22d6fdd6
Thalisson Castro <adaothalisson@hotmail.com>
2024-12-20 21:04:38 -0300

    feat: criado o menu para o usuário escolher a opção de conversão

--- src/size_conversion.c
+++ src/size_conversion.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 void showSizeConversionMenu() {
-    printf("Menu de Conversor de Comprimento\n");
+    int choice;
+
+    do{
+        printf("\n--- Conversor de Comprimento ---\n");
+        printf("1. Converter metros para centímetros\n");
+        printf("2. Converter metros para milímetros\n");
+        printf("3. Converter centímetros para metros\n");
+        printf("4. Converter centímetros para milímetros\n");
+        printf("5. Converter milímetros para metros\n");
+        printf("6. Converter milímetros para centímetros\n");
+        printf("7. Voltar\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &choice);
+    }while(choice != 7);
 }
