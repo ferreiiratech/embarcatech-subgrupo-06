40ecb28105d1753277bc14613bb84afa563374ef
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-12-22 21:09:59 -0300

    :bug: fix: consertando problema de compatibilidade da acentuação

--- src/potencia_conversion.c
+++ src/potencia_conversion.c
@@ -1,4 +1,4 @@
-// Conversão de unidade de potência
+// ConversÃ£o de unidade de potÃªncia
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,42 +9,44 @@ void ConversaoPotencia( char unidadep, float valorp) {
     
     switch(unidadep) {
     case 'w':
-        printf("A potência é: %.2fW, %.2fkW, %.2fcv e %.2fhp\n", valorp, valorp/1000, valorp/735.5, valorp/745.7);
+        printf("A potÃªncia Ã©: %.2fW, %.2fkW, %.2fcv e %.2fhp\n", valorp, valorp/1000, valorp/735.5, valorp/745.7);
         break;
 
     case 'k':
-        printf("A potência é: %.2fkW, %.2fW, %.2fcv, %.2fhp\n", valorp, valorp*1000, valorp*1000 / 735.5, valorp*1000/745.7);
+        printf("A potÃªncia Ã©: %.2fkW, %.2fW, %.2fcv, %.2fhp\n", valorp, valorp*1000, valorp*1000 / 735.5, valorp*1000/745.7);
         break;
 
     case 'c':
-        printf("A potência é: %.2fcv, %.2fW, %.2fkW, %.2fhp\n", valorp, valorp* 735.5, valorp* 735.5/1000, valorp*735.5/745.7);
+        printf("A potÃªncia Ã©: %.2fcv, %.2fW, %.2fkW, %.2fhp\n", valorp, valorp* 735.5, valorp* 735.5/1000, valorp*735.5/745.7);
         break;
 
     case 'h':
-        printf("A potência é: %.2fhp, %.2fW, %.2fkW, %.2fcv\n", valorp, valorp*745.7, valorp*745.7/1000, valorp*745.7/735.5);
+        printf("A potÃªncia Ã©: %.2fhp, %.2fW, %.2fkW, %.2fcv\n", valorp, valorp*745.7, valorp*745.7/1000, valorp*745.7/735.5);
         break;
 
     default:
-        printf("Unidade inválida. Por favor, insira w, k, c ou h.\n"); }
-    }
+        printf("Unidade invÃ¡lida. Por favor, insira w, k, c ou h.\n"); }
+}
+
 int main() {
- setlocale(LC_ALL, "portuguese");
- char unidadep, continuar;
- float valorp;
+    setlocale(LC_ALL, "portuguese");
+    char unidadep, continuar;
+    float valorp;
 
-do {
-    printf("Insira a unidade de potência: 'w' para watts, 'k' para kilo watts, 'c' para cavalo vapor, 'h' para Hourse Power\n");
-    scanf(" %c", &unidadep);
-    
-    printf("Insira o valor da potência\n");
-    scanf("%f", &valorp);
-    
-    ConversaoPotencia(unidadep, valorp);
-    
-    printf("\nDigite 's' caso deseje continuar e 'n' caso deseja encerrar\n");
-    scanf(" %c", &continuar); }
+    do {
+        printf("Insira a unidade de potÃªncia: 'w' para watts, 'k' para kilo watts, 'c' para cavalo vapor, 'h' para Hourse Power\n");
+        scanf(" %c", &unidadep);
+        
+        printf("Insira o valor da potÃªncia\n");
+        scanf("%f", &valorp);
+        
+        ConversaoPotencia(unidadep, valorp);
+        
+        printf("\nDigite 's' caso deseje continuar e 'n' caso deseja encerrar\n");
+        scanf(" %c", &continuar); 
+    } while (continuar == 's' || continuar == 'S');
 
-while (continuar == 's' || continuar == 'S');
-printf("\nprograma encerrado\n");
-system("pause");
-return 0; }
+    printf("\nprograma encerrado\n");
+    system("pause");
+    return 0; 
+}
