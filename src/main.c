7ad47316ea71b61959ef15279bd75f4f039909b5
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-12-22 21:34:30 -0300

    :bug: fix: adiciona break após a execução da função showSpeedConversionMenu

--- src/main.c
+++ src/main.c
@@ -35,6 +35,7 @@ int main() {
                 break;
             case 5:
                 showSpeedConversionMenu();
+                break;
             case 6:
                 showPotenciaConversionMenu();
                 break;
