585fc2b69e8fb22266c5ace2b05d59ae5c8351dd
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-12-25 17:23:11 -0300

    :bug: fix: ajustes e adaptações para integrar a funcionalidade ao menu principal

--- src/area_conversion.c
+++ src/area_conversion.c
@@ -6,37 +6,35 @@
 void areaConversao(char unidade, float valor) {
     switch (unidade) {
         case 'm':
-        printf("O valor da ·rea È: \n%.3f m^2 \n%.3f cm^2 \n", valor, valor*10000);
-        break;
-
+            printf("O valor da √°rea √©: \n%.3f m^2 \n%.3f cm^2 \n", valor, valor*10000);
+            break;
         case 'c':
-        printf("O valor da ·rea È: \n%.3f m^2 \n%.3f cm^2\n", valor/10000, valor);
-        break;
-
+            printf("O valor da √°rea √©: \n%.3f m^2 \n%.3f cm^2\n", valor/10000, valor);
+            break;
         default:
-        printf("Unidade inv·lida!");
-        break; } }
+            printf("Unidade inv√°lida!");
+            break; 
+    } 
+}
 
-        int main() {
-            float valor;
-            char unidade, continuar;
+void showAreaConversionMenu() {
+    float valor;
+    char unidade, continuar;
 
-            setlocale(LC_ALL, "portuguese");
+    do {
+        system("cls");
+        printf("Qual unidade deseja converter? \nDigite m - para converter de metros quadrados para cent√≠metros quadrados \ndigite c - para converter de cent√≠metros para metros quadrados\n");
+        scanf(" %c", &unidade);
 
-            do {
-                printf("Qual unidade deseja converter? \nDigite m - para converter de metros quadrados para centÌmetros quadrados \ndigite c para converter de centimetros para metros quadrados\n");
-                scanf(" %c", &unidade);
+        printf("Digite o valor da unidade de √°rea: ");
+        scanf(" %f", &valor);
 
-                printf("Digite o valor da unidade de ·rea: ");
-                scanf(" %f", &valor);
+        areaConversao( unidade, valor);
 
-                areaConversao( unidade, valor);
-
-                printf("\nDigite 's' para continuar ou outra tecla para encerrar.\n");
-                scanf(" %c", &continuar); }
-
-                while (continuar == 's' || continuar == 'S');
-                printf("Voltando...\n");
-
-                system("pause");
-                return 0; }
+        printf("\nDigite 's' para continuar ou outra tecla para encerrar.\n");
+        scanf(" %c", &continuar); 
+    } while (continuar == 's' || continuar == 'S');
+    
+    printf("\nVoltando...\n");
+    system("pause");
+}
